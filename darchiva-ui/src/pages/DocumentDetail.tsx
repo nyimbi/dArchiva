346bf9c8b2a578cff381acc359786f31761a0a9c
Nyimbi Odero <nyimbi@gmail.com>
2026-06-17 13:04:23 +0300

    fix: remove undeclared documentTitle prop from WatermarkDialog

--- darchiva-ui/src/pages/DocumentDetail.tsx
+++ darchiva-ui/src/pages/DocumentDetail.tsx
@@ -424,7 +424,6 @@ export function DocumentDetail() {
 				<WatermarkDialog
 					open={showWatermarkDialog}
 					documentId={id!}
-					documentTitle={document?.title ?? ''}
 					onClose={() => setShowWatermarkDialog(false)}
 				/>
 			)}
